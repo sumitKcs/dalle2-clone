5291e63b6740142afafe6e11b425148de94403f7
Sumit Singh Rajput <itssumit258@gmail.com>
2023-02-27 18:54:27 +0530

    update image lazy loading

--- components/Card.tsx
+++ components/Card.tsx
@@ -23,6 +23,7 @@ function Card({ _id, post }: data) {
           <div className="flex items-center gap-2">
             <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">
               <img
+                loading="lazy"
                 className="rounded-full"
                 src={post?.user?.avatar}
                 alt={post?.user?.name}
