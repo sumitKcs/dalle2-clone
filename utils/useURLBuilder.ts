dd5a3f5857cff5e210748c6c6dcbab878ff65ed1
Sumit Singh Rajput <itssumit258@gmail.com>
2023-03-08 21:42:11 +0530

    custom hook useURLBuilder

--- utils/useURLBuilder.ts
+++ utils/useURLBuilder.ts
@@ -3,15 +3,13 @@ import { useState, useEffect } from "react";
 
 const useURLBuilder = (url:string, query: string, format:string ): string => {
     const [imgURL, setImgURL] = useState<string> ()
-    console.log("useURLBuilder called")
 
     useEffect(() => {
-        console.log("useURLBuilder Useeffect called")
-      
+
         let imgArray = url.split("/");
-        imgArray.splice(6, 0, "q_70,w_400");
+        imgArray.splice(6, 0, query);
         const getLastElement = imgArray[imgArray.length - 1];
-        const getSecondLastElement = getLastElement.split(".")[0] + ".avif";
+        const getSecondLastElement = getLastElement.split(".")[0] + format;
         imgArray[imgArray.length - 1] = getSecondLastElement;
         let newUrl = imgArray.join("/");
         setImgURL(newUrl)
